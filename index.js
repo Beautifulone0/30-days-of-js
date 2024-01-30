5f92eec9295978f035d75e857e89177b1d7b8cf3
beautifulone0 <konyeanifaith@gmail.com>
2024-01-30 21:53:13 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,3 @@
+// comments can make code readable
+// welcome to 30 days of javascript
+/* comment can make code readable,easy to resue and informative*/
---
+++ variable.js
@@ -0,0 +1,10 @@
+let langauge = 'JavaScript';
+let iseasy = false;
+let learning 
+let numb = null;
+console.log(numb);
+
+console.log(typeof langauge)
+console.log(typeof iseasy)
+console.log(typeof learning)
+console.log(typeof numb);
