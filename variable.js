2657b6f5d4789280d625996cccf6ea62beb22467
beautifulone0 <konyeanifaith@gmail.com>
2024-01-30 22:32:48 +0100

    javascript

--- variable.js
+++ variable.js
@@ -8,3 +8,34 @@ console.log(typeof langauge)
 console.log(typeof iseasy)
 console.log(typeof learning)
 console.log(typeof numb);
+
+
+//Declare four variables without assigning values
+let date
+let job
+let season
+let time
+
+//Declare four variables with assigned values
+let names = 'faith, daisy'
+let year = 2024
+let month = 'january'
+let islearning = true
+
+//Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'faith kalu'
+let lastName = 'onyeani'
+let maritalStatus = 'single'
+let country = 'nigeria'
+let age= 24
+
+//Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let Fname = 'faith kalu', lname = 'onyeani', mS = 'single', C = 'nigeria', A = '24'
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 24
+let yourAge = 22
+console.log('i am' +" " + myAge 
++" " + 'years old' +" " + 'you are'+ " " + yourAge + 
+" " + "years old" )
